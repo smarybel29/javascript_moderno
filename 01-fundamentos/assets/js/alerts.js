65d53dc87cf51dc0c230e24a3958311781f018e5
Sonia <coffeedevgirl@gmail.com>
2021-07-27 21:44:24 -0500

    Cap 1&2 fundamentos

---
+++ 01-fundamentos/assets/js/alerts.js
@@ -0,0 +1,9 @@
+// console.log(miNombre);
+
+/*alert('Hola mundo');
+let nombre = prompt('Cual es tu nombre?');
+console.log(nombre);
+console.log('****' + nombre + '****');*/
+// const seleccion = confirm('Estas seguro de borrar esto?');
+// console.log(seleccion);
+console.log(global);
---
+++ 01-fundamentos/assets/js/app.js
@@ -0,0 +1,30 @@
+//console.log('Hola mundo');
+
+// let a = 10;
+// var b = 10;
+// const c = 10;
+// alert("Hola mundo");
+
+//  esto no se puede hacer c = 20;
+
+let a = 10,
+    b = 20,
+    c = 'Hola ',
+    d = 'Spiderman',
+    x = a + b;
+
+const saludo = c + d;
+// esta es la forma larga
+// console.log('a', a);
+// console.log('b', b);
+// console.log('c', c);
+
+// esta es la forma corta
+/* console.log('%c Mis variables', 'color:blue; font-weight:bold');
+console.log({ a });
+console.log({ b });
+console.log({ c }); */
+c = 'Hola de nuevo';
+// console.table({ a, b, c, d, x });
+let outerWidth = 1000000;
+const outerHeight = 600;
